bb9e0fb1fd6e5f9b1d7daa023b5bec0cebb40566
Zach Dyer <zach.dyer@zachdyerdesign.com>
2016-02-22 08:31:29 -0600

    Initial files

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+  "net/http"
+  "io/ioutil"
+  "log"
+  "strings"
+)
+
+type MyHandler struct {
+}
+
+func (this *MyHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request){
+  path := req.URL.Path[1:]
+  log.Println(path)
+
+  data, err := ioutil.ReadFile(string(path))
+
+  if err == nil {
+    var contentType string
+
+    if strings.HasSuffix(path, ".css") {
+      contentType = "text/css"
+    } else if strings.HasSuffix(path, ".html") {
+      contentType = "text/html"
+    } else if strings.HasSuffix(path, ".js") {
+      contentType = "application/javascript"
+    } else if strings.HasSuffix(path, ".png") {
+      contentType = "image/png"
+    } else {
+      contentType = "text/plain"
+    }
+
+    rw.Header().Set("Content-Type", contentType)
+    rw.Write(data)
+  } else {
+    rw.WriteHeader(404)
+    rw.Write([]byte("404 My Friend - " + http.StatusText(404)))
+  }
+
+}
+
+func main() {
+  http.Handle("/", new(MyHandler))
+  http.ListenAndServe(":8080", nil)
+}
